47f2d5b2cd4ebbabec1f4bf5e52ab967e2ae0aee
Patcharapong Jongprasirth <znqgre6187@gmail.com>
2025-05-31 15:43:20 +0700

    Update main.py

--- NumMoveMouse/main.py
+++ NumMoveMouse/main.py
@@ -5,23 +5,8 @@ root = Tk()
 root.title("NumMoveMouse")
 root.geometry("640x400")
 
-frame = Frame(root)
-frame.pack()
-
 custom_font = Font(family="Arial", size=20)
 
 nametitle = Label(root, text="NumMoveMouse", fg="black", font=custom_font, pady=12).pack()
-
-userinfo = int
-
-move_left = Entry(userinfo)
-move_right = Entry(userinfo)
-move_up = Entry(userinfo)
-move_down = Entry(userinfo)
-
-move_left.grid(row=2, column=1).pack()
-move_right.grid(row=2, column=3).pack()
-move_up.grid(row=1, column=0).pack()
-move_down.grid(row=2, column=2).pack()
 
 root.mainloop()
