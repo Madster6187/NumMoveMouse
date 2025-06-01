d84180fb0d16a97306e14dd98ace67db39c7e788
Patcharapong Jongprasirth <znqgre6187@gmail.com>
2025-06-01 20:09:17 +0700

    Add move mouse function main.cpp

--- NumMoveMouse/main.cpp
+++ NumMoveMouse/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <stdio.h>
 #include <window.h>
 using namespace std;
+
+bool numlock_activate = false;
+
+void moveMouse(int x, int y){
+    INPUT input = {0};
+    input.type = INPUT_MOUSE;
+    
+}
 
 int main(){
 
     while (true){
-
+        
         
     }
 }
