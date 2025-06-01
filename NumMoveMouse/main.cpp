69fa689bc75cfbe6927c3156796f856e49b220ee
Patcharapong Jongprasirth <znqgre6187@gmail.com>
2025-06-01 20:12:00 +0700

    Update moveMouse() fucntion in main.cpp

--- NumMoveMouse/main.cpp
+++ NumMoveMouse/main.cpp
@@ -6,8 +6,12 @@ using namespace std;
 bool numlock_activate = false;
 
 void moveMouse(int x, int y){
-    INPUT input = {0};
+    INPUT input = { 0 };
     input.type = INPUT_MOUSE;
+    input.mi.dx = (x * 65535) / GetSystemMetrics(SM_CXSCREEN);
+    input.mi.dy = (y * 65535) / GetSystemMetrics(SM_CYSCREEN);
+    input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+    SendInput(1, &input, sizeof(INPUT));
     
 }
 
