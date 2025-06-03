f26daf0510b170fb65b63abdac7eb85f892a3f0b
Patcharapong Jongprasirth <znqgre6187@gmail.com>
2025-06-03 08:40:09 +0700

    add keyboard input and update movemouse function

--- NumMoveMouse/main.cpp
+++ NumMoveMouse/main.cpp
@@ -3,17 +3,27 @@
 #include <window.h>
 using namespace std;
 
-bool numlock_activate = false;
+bool activate = false;
 
-float spd;
+int spd, refresh_rate;
 
-void moveMouse(float dx, float dy){
-    mouse_event(MOUSEEVENTF_MOVE, dx, dy, 0, 0);
+void moveMouse(float dx, float dy) {
+    INPUT input = {0};
+    input.type = INPUT_MOUSE;
+    input.mi.dx = static_cast<LONG>(dx);
+    input.mi.dy = static_cast<LONG>(dy);
+    input.mi.dwFlags = MOUSEEVENTF_MOVE; // Relative move
+    SendInput(1, &input, sizeof(INPUT));
 }
 
 int main(){
-
+    
     while (true){
+        if (GetKeyState(VK_NUMPAD4) & 0x8000) {moveMouse(-spd, 0);} // left
+        if (GetKeyState(VK_NUMPAD6) & 0x8000) {moveMouse(spd, 0);}  // right
+        if (GetKeyState(VK_NUMPAD8) & 0x8000) {moveMouse(0, -spd);} // up
+        if (GetKeyState(VK_NUMPAD5) & 0x8000) {moveMouse(0, spd);}  // down
         
+        if (GetKeyState('T') & 0x8000) {return 0;} // end program
     }
 }
